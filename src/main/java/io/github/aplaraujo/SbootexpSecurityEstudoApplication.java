a000927d4a254e71813ac59fc5738ac11469a8ef
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-09 21:05:35 -0300

    first commit

---
+++ src/main/java/io/github/aplaraujo/SbootexpSecurityEstudoApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootexpSecurityEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootexpSecurityEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aplaraujo/SbootexpSecurityEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootexpSecurityEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
